3dde546c83c69eaf6084c8a3d9d93382ce91ae59
bomen-ulrich <109666171+bomen-ulrich@users.noreply.github.com>
2024-01-13 13:26:19 -0700

    Add files via upload
    Initial files to work on

---
+++ main.tf
@@ -0,0 +1,28 @@
+#Write the main code definition here to create VPC - Subnets - ECR
+
+# VPC definition
+resource "aws_vpc" "ori_vpc" {
+    cidr_block = var.vpc_cidr
+    enable_dns_hostnames = true
+    enable_dns_support = true 
+    tags = {
+    Name = "ori wordpress app vpc"
+    environment = "dev"
+	  project = "ori-wordpress-docker-ecr-ecs"
+	  terraform = true
+    }
+}
+#Created  Subnet 1
+resource "aws_subnet" "subnet1" {
+    vpc_id = "${aws_vpc.ori_vpc.id}" #we specify on which VPC to create the subnet
+    cidr_block = var.sub1_cidr #"10.0.0.0/24" #we specify the number of IP address available 
+    availability_zone = var.az2a # var.dev_AZ1 # we specify the AZ where to create the subnet
+    #availability_zone = data.aws_availability_zones.available.names[0]
+    map_public_ip_on_launch = true
+  tags = {
+    Name = "ori public subnet 1"
+    environment = "dev"
+		project = "ori-wordpress-docker-ecr-ecs"
+		terraform = true
+  }   
+}
---
+++ providers.tf
@@ -0,0 +1,16 @@
+#Write the provider code here
+terraform {
+  required_providers {
+	aws = {
+	  source  = "hashicorp/aws"
+	  version = "~> 3.46.0"
+	}
+	random = {
+	  source  = "hashicorp/random"
+	  version = "~> 3.1.0"
+	}
+  }
+}
+provider "aws" {
+  region = var.app-region
+}
